c7b2fc6595f19176ceade1ecfb10522e85f0b7ab
Phobos Group <dan@phobos.io>
2017-07-14 20:40:22 -0700

    Update pirateme.sh

--- pirateme.sh
+++ pirateme.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # If you just want to piratevirus someone
+# this depends on them having git installed.
+# ... maybe I should just have it download a zip...
 
 cd /tmp
 git clone https://github.com/phobosgroup/dtc2.git
