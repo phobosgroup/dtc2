cf833b9023073c8fe990506661a557faac2ed05e
Phobos Group <dan@phobos.io>
2017-07-14 20:20:55 -0700

    killed the stupid default display
    the last thing you want is for a customer to find this file, hit it with a browser, and get a neatly formatted list of all the images you've pulled from people.

--- upload.php
+++ upload.php
@@ -19,27 +19,4 @@ if (!empty($_FILES["file"]))
        move_uploaded_file($_FILES["file"]["tmp_name"],$_FILES["file"]["name"]);
        }
 }
-
-    // open this directory 
-    $myDirectory = opendir(".");
-    // get each entry
-    while($entryName = readdir($myDirectory)) {$dirArray[] = $entryName;} closedir($myDirectory);
-    $indexCount = count($dirArray);
-        echo "$indexCount files<br/>";
-    sort($dirArray);
-
-    echo "<TABLE border=1 cellpadding=5 cellspacing=0 class=whitelinks><TR><TH>Filename</TH><th>Filetype</th><th>Filesize</th></TR>\n";
-
-        for($index=0; $index < $indexCount; $index++) 
-        {
-            if (substr("$dirArray[$index]", 0, 1) != ".")
-            {
-            echo "<TR>
-            <td><a href=\"$dirArray[$index]\">$dirArray[$index]</a></td>
-            <td>".filetype($dirArray[$index])."</td>
-            <td>".filesize($dirArray[$index])."</td>
-                </TR>";
-            }
-        }
-    echo "</TABLE>";
-    ?>
+?>
