8a9c39a4b240dcc415f272c6084d71b0cfc16eed
Viss <dan@atenlabs.com>
2017-09-13 16:59:41 -0700

    Updated this script so it runs one-shot
    removed calls to git, made it pull the zip.
    fixed paths

--- pirateme.sh
+++ pirateme.sh
@@ -5,8 +5,9 @@
 # ... maybe I should just have it download a zip...
 
 cd /tmp
-git clone https://github.com/phobosgroup/dtc2.git
-cd dtc2
+curl -o pirate.zip https://codeload.github.com/phobosgroup/dtc2/zip/master
+unzip pirate.zip
+cd dtc2-master
 mv pirate ../piratefiles
 cd ../
 rm -rf dtc2
